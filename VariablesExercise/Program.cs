fd969e49d15b4dea7434634b11a5ca521c7fb4eb
Shaun Rudisail <sprudisail@gmail.com>
2025-06-06 22:07:44 -0400

    Changes made to Variables Exercise have been saved and added.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace VariablesExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Shaun";
+            int myAge = 35;
+            char myMiddleInitial = 'P';
+            bool iPlayGuitar = true;
+            bool iHaveToured = false;
+            double myHeight = 71;
+            double myWeight = 160;
+            decimal myNetIncome = 41600;
+
+            Console.WriteLine($"Hello there! My name is {myName} and I'm {myAge} years old. My middle initial is {myMiddleInitial}.");
+            Console.WriteLine($"I'm {myHeight} inches tall and I weigh {myWeight} pounds.");
+            Console.WriteLine($"It's {iPlayGuitar} that I am an avid guitar player. However, it's {iHaveToured} that I have ever toured playing music.");
+            Console.WriteLine($"A rough estimation of my net income is ${myNetIncome}. Taxes are a killer. I wish that I could make ${myNetIncome} from touring around the country playing music!");
         }
     }
 }
